ad810b69996f3de20a9ebac5dac67f9ab97455aa
DobrovIgnat <ignat.dobrov@gmail.com>
2025-11-09 22:08:46 +0300

    Delete git_setup.bat

--- git_setup.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set /p name="Your GitHub username: "
-set /p email="Your GitHub email: "
-
-git config --global user.name "%name%"
-git config --global user.email "%email%"
-
-echo Git configured for: %name%
-pause
